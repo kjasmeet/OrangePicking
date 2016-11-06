a2b759e51a57ac30aeef213c52b106f97716bff3
anlewandowski <alina.n.lewandowski@gmail.com>
2016-11-06 16:46:23 -0500

    Added text to screen

--- src/orangepickinggame/Play.java
+++ src/orangepickinggame/Play.java
@@ -1,9 +1,15 @@
 package orangepickinggame;
 
+import java.awt.FontFormatException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import static orangepickinggame.OrangePickingGame.play;
 import static orangepickinggame.OrangePickingGame.playLevelTwo;
 import org.newdawn.slick.*;
 import org.newdawn.slick.state.StateBasedGame;
+import org.newdawn.slick.util.ResourceLoader;
 
 /**
  *
@@ -24,25 +30,28 @@ public class Play extends InitializeCode {
     
     @Override
     public void enter(GameContainer gc, StateBasedGame sbg){
-        super.setTime(90000);
+        super.setTime(60000);
         super.score = 0;
     }
     
     @Override
     public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
         super.init(gc, sbg);
+
     }
 
     @Override
     public void render(GameContainer gc, StateBasedGame sbg, Graphics grphcs) throws SlickException {
         super.render(gc, sbg, grphcs);
+        font.drawString(50, 20, "Level 1 - Pick oranges!", Color.yellow);
+        grphcs.setColor(Color.yellow);
     }
 
     @Override
     public void update(GameContainer gc, StateBasedGame sbg, int i) throws SlickException {
         super.update(gc, sbg, i);
         
-        if(orangeGoal <= super.score){
+        if(super.getTime() <= 0){
             sbg.enterState(playLevelTwo);
         }
     }
